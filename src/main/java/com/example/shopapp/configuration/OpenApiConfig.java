5a9619bf11d9e780bbc529862ad988503b79402c
thduy2003 <duytrieudong@gmail.com>
2024-03-11 15:22:35 +0700

    up

--- src/main/java/com/example/shopapp/configuration/OpenApiConfig.java
+++ src/main/java/com/example/shopapp/configuration/OpenApiConfig.java
@@ -30,6 +30,10 @@ import io.swagger.v3.oas.annotations.servers.Server;
                 @Server(
                         description = "Local ENV",
                         url = "http://localhost:8088"
+                ),
+                @Server(
+                        description = "Build production",
+                        url = "https://springboot-shopapp.onrender.com"
                 )
 
         },
